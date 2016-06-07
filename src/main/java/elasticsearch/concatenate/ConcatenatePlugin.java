7bb9e679383bf177c7061114f3f9d00c12ea2de7
Craig <craig@blubolt.com>
2016-06-07 11:26:26 +0100

    General code cleanup

--- src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
+++ src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
@@ -1,24 +1,24 @@
 package org.elasticsearch.index.analysis.concatenate;
 
-import java.util.Collection;
 import org.elasticsearch.index.analysis.AnalysisModule;
-
-import org.elasticsearch.common.component.LifecycleComponent;
-import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.plugins.Plugin;
 
-public class ConcatenatePlugin extends Plugin {
-    @Override
-    public String name() {
-        return "analysis-concatenate";
-    }
+public class ConcatenatePlugin extends Plugin
+{
+	@Override
+	public String name()
+	{
+		return "analysis-concatenate";
+	}
 
-    @Override
-    public String description() {
-        return "Plugin that provides a Token Filter that recombines all of the tokens in a token stream back into one.";
-    }
+	@Override
+	public String description()
+	{
+		return "Plugin that provides a Token Filter that recombines all of the tokens in a token stream back into one.";
+	}
 
-    public void onModule(AnalysisModule module) {
-        module.addTokenFilter("concatenate", ConcatenateTokenFilterFactory.class);
-    }
+	public void onModule(AnalysisModule module)
+	{
+		module.addTokenFilter("concatenate", ConcatenateTokenFilterFactory.class);
+	}
 }
